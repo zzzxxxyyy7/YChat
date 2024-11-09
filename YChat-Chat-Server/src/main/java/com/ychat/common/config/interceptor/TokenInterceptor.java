b312d34ecf9606406011bbd738ebc29c576f2c39
Rhss <3567878896@qq.com>
2024-11-10 03:14:10 +0800

    开发 Token 拦截器，接口访问登录鉴权

---
+++ YChat-Chat-Server/src/main/java/com/ychat/common/config/interceptor/TokenInterceptor.java
@@ -0,0 +1,66 @@
+package com.ychat.common.config.interceptor;
+
+import com.ychat.common.Error.HttpErrorEnum;
+import com.ychat.common.user.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * 提取 Token 拦截器
+ */
+@Component
+public class TokenInterceptor implements HandlerInterceptor {
+
+    public static final String TOKEN_HEADER = "Authorization";
+    public static final String BEARER = "Bearer ";
+
+    @Autowired
+    private LoginService loginService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String token = getToken(request);
+        Long validUid = loginService.getValidUid(token);
+        if (Objects.nonNull(validUid)) {
+            request.setAttribute("uid", validUid);
+        } else {
+            boolean privateURI = isPrivateURI(request, response);
+            if (!privateURI) {
+                HttpErrorEnum.ACCESS_DENIED.sendHttpError(response);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 如果为 private，表示必须登录才可以访问，返回 false
+     * @param request
+     * @return
+     */
+    private boolean isPrivateURI(HttpServletRequest request , HttpServletResponse response) {
+        String requestURI = request.getRequestURI();
+        String[] URIArray = requestURI.split("/");
+        List<String> URIList = Arrays.stream(URIArray).collect(Collectors.toList());
+        return !URIList.contains("private");
+    }
+
+    private String getToken(HttpServletRequest request) {
+        String token = Optional.ofNullable(request.getHeader(TOKEN_HEADER))
+                .filter(h -> h.startsWith(BEARER))
+                .map(h -> h.replaceFirst(BEARER, ""))
+                .orElse(null);
+        return token;
+    }
+
+
+}
