c48c16e28a977e5b38c89a8495054be539e1bcb6
Rhss <3567878896@qq.com>
2024-11-16 11:25:40 +0800

    已修复 404 Bug

--- YChat-Chat-Server/src/main/java/com/ychat/common/user/controller/BlackController.java
+++ YChat-Chat-Server/src/main/java/com/ychat/common/user/controller/BlackController.java
@@ -1,6 +1,5 @@
 package com.ychat.common.user.controller;
 
-
 import com.ychat.common.Enums.RoleEnum;
 import com.ychat.common.front.Response.ApiResult;
 import com.ychat.common.user.domain.dto.BlackReq;
@@ -8,21 +7,18 @@ import com.ychat.common.user.service.IBlackService;
 import com.ychat.common.user.service.IRoleService;
 import com.ychat.common.utils.Assert.AssertUtil;
 import com.ychat.common.utils.Request.RequestHolder;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
- * 黑名单 前端控制器
- *
- * @author <a href="https://github.com/zongzibinbin">Rhss</a>
- * @since 2024-11-14
+ * 用户表 前端控制器
+ * @since 2024-11-04
  */
-@Controller
+@RestController
 @RequestMapping("/black")
+@Api(tags = "用户相关")
 public class BlackController {
 
     @Autowired
@@ -31,23 +27,31 @@ public class BlackController {
     @Autowired
     private IBlackService blacksService;
 
+    @GetMapping("/private/test")
+    @ApiOperation("测试")
+    public ApiResult<Void> test() {
+        return ApiResult.success();
+    }
+
     @PostMapping("/private/blackUid")
     @ApiOperation("拉黑uid")
-    public ApiResult<Void> blackUid(@RequestBody BlackReq req) {
+    public ApiResult<?> blackUid(@RequestBody BlackReq req) {
         Long uid = RequestHolder.get().getUid();
         boolean hasRole = roleService.hasRole(uid, RoleEnum.ADMIN);
         AssertUtil.isTrue(hasRole, "没有权限");
         blacksService.blackUid(req);
-        return ApiResult.success();
+        return ApiResult.success("拉黑成功");
     }
 
     @PostMapping("/private/blackIp")
     @ApiOperation("拉黑Ip")
-    public ApiResult<Void> blackIp(@RequestBody BlackReq req) {
+    public ApiResult<?> blackIp(@RequestBody BlackReq req) {
         Long uid = RequestHolder.get().getUid();
         boolean hasRole = roleService.hasRole(uid, RoleEnum.ADMIN);
         AssertUtil.isTrue(hasRole, "没有权限");
         blacksService.BlackIp(req.getIp());
-        return ApiResult.success();
+        return ApiResult.success("拉黑成功");
     }
+
 }
+
