d508a2f581804dbfb6b142ad2672293ee5db875a
Rhss <3567878896@qq.com>
2024-11-24 23:04:24 +0800

    引入 本地消息表实体类

---
+++ YChat-Chat-Server/src/main/java/com/ychat/common/user/mapper/SecureInvokeRecordMapper.java
@@ -0,0 +1,16 @@
+package com.ychat.common.user.mapper;
+
+import com.ychat.common.user.domain.entity.SecureInvokeRecord;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 本地消息表 Mapper 接口
+ * </p>
+ *
+ * @author ${author}
+ * @since 2024-11-24
+ */
+public interface SecureInvokeRecordMapper extends BaseMapper<SecureInvokeRecord> {
+
+}
---
+++ YChat-Chat-Server/src/main/java/com/ychat/common/user/service/ISecureInvokeRecordService.java
@@ -0,0 +1,9 @@
+package com.ychat.common.user.service;
+
+
+/**
+ * 本地消息表 服务类
+ */
+public interface ISecureInvokeRecordService {
+
+}
